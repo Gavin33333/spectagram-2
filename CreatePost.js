46a929f14d9b5947ee17af107b660b95080ebd11
Gavin33333 <70176966+Gavin33333@users.noreply.github.com>
2021-07-28 08:32:00 -0600

    Add files via upload

---
+++ CreatePost.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Component } from 'react/cjs/react.production.min';
+
+export default class CreatePostScreen extends Component{
+    render(){
+        return(
+            <Text>Create A Post Here</Text>
+        )
+    }
+}
